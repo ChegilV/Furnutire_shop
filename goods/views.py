1e8918f1755ecff6a133022dbe9ce5112efd83a4
chegil <vovachegil@gamil.com>
2023-12-26 14:26:24 +0200

    second pagination option GET-params

--- goods/views.py
+++ goods/views.py
@@ -5,15 +5,17 @@ from goods.models import Products
 
 
 # Create your views here.
-def catalog(request, category_slug, page=1):
- 
+def catalog(request, category_slug):
+    
+    page = request.GET.get('page', 1)
+    
     if category_slug == 'all-products':
            goods = Products.objects.all()
     else:
          goods = get_list_or_404(Products.objects.filter(caategory__slug=category_slug))
         
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - catalog",
