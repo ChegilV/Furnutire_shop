4b2a1bb5c4ecb057a8e6a767400baef5653dc457
chegil <vovachegil@gamil.com>
2023-12-28 22:03:48 +0200

    Login form and controller are ready

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', null=True,
+                              verbose_name='profile-image')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self) -> str:
+        return self.username
