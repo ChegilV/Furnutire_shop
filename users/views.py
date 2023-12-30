755e1260329ef204a280089a101895b9cb78cd5b
chegil <vovachegil@gamil.com>
2023-12-30 11:06:24 +0200

    Session-carts for annonym user

--- users/views.py
+++ users/views.py
@@ -4,10 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from carts.models import Cart
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
-from django.contrib import auth, messages
+from django.contrib import auth, messages, sessions
 
 # Create your views here.
 def login(request):
@@ -18,9 +19,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username},You have Successfuly logged in')
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -45,8 +52,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+           
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+           
             messages.success(request, f'{user.username} You have successfuly registered')
            
             return HttpResponseRedirect(reverse('main:index'))
